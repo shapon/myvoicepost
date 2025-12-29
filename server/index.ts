3e25bf673f5e82a6c7a6aa237fb8a98f25d5d34d
sdussa <dsreekrishna@gmail.com>
2025-12-29 09:42:26 +0530

    ADDED LIMIT

--- server/index.ts
+++ server/index.ts
@@ -15,13 +15,14 @@ declare module "http" {
 
 app.use(
   express.json({
+    limit: '50mb',
     verify: (req, _res, buf) => {
       req.rawBody = buf;
     },
   }),
 );
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 
 // Session middleware
 app.use(
