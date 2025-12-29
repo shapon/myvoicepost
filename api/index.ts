a00c341727d823ee03f760afae14ac8450c64971
sdussa <dsreekrishna@gmail.com>
2025-12-29 10:05:32 +0530

    updated limit

--- api/index.ts
+++ api/index.ts
@@ -261,8 +261,8 @@ app.use(cors({
   credentials: true,
 }));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 
 const upload = multer({
   storage: multer.memoryStorage(),
